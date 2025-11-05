a0835d5b4cf7829c18c38993ae8007206260fba3
goodtu02 <qwe13117@gmail.com>
2025-11-05 13:50:16 +0900

    Fix. setupProxy

--- src/main/aegis-ai-frontend/src/setupProxy.js
+++ src/main/aegis-ai-frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: 'http://http://15.164.13.20:8080/',
     changeOrigin: true,
 })
 );
