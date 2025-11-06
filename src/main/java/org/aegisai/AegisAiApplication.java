9d1c2fae6a7d032fb7a11558540a5ecdca313394
goodtu02 <qwe13117@gmail.com>
2025-11-06 18:16:37 +0900

    Fix.AegisAiApplication

--- src/main/java/org/aegisai/AegisAiApplication.java
+++ src/main/java/org/aegisai/AegisAiApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class
-})
+@SpringBootApplication
 public class AegisAiApplication {
 
     public static void main(String[] args) {
